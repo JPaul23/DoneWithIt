cb8ec422f799078fe8e03134cda8f4e8478d4038
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-01-03 16:50:46 +0200

    Topic: Welcome screen, ViewImageScreen & Refactoring

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: 'white',
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center", //main
-      alignItems: "center", // secondary, Alignment on each line
-      alignContent: 'center', // Alignment of entire content,use when there is wrapping.
-      flexWrap: "wrap",
-    }}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: 100,
-        height: 100,
-      }}
-      />
-      <View style={{
-        backgroundColor: 'gold',
-        width: 100,
-        height: 100,
-      }}
-      />
-      <View style={{
-        backgroundColor: 'tomato',
-        width: 100,
-        height: 100,
-        top: 20,
-        left: 20,
-        position: 'absolute'
-      }}
-      />
-      <View style={{
-        backgroundColor: 'grey',
-        width: 100,
-        height: 100,
-      }}
-      />
-    </View>
+    <ViewImageScreen />
+
   );
 }
