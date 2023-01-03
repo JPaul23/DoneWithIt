c0c67c9e20add340865c108ebc6a360057130423
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-01-03 12:33:21 +0200

    Topics covered, View, text & Image

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console.log('App executed!');
+  const handlePress = () => console.log('Text pressed!');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>
+        Hello React Native!
+      </Text>
+      <Image
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"
+        }} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
