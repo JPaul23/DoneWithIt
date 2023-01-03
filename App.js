b2c1f762687dd52965144ed66734464a53e0c4b6
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-01-03 14:08:11 +0200

    Topic: Buttons, Alerts, Stylesheet

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableNativeFeedback } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
-  console.log('App executed!');
-  const handlePress = () => console.log('Text pressed!');
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>
-        Hello React Native!
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log('Image tapped!')} >
-        <Image
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"
-          }} />
-      </TouchableNativeFeedback>
-      <TouchableNativeFeedback>
-        <View style={{ width: 200, height: 70, backgroundColor: 'blue' }}></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button
+        title='Click Me!'
+        color="orange"
+        onPress={() =>
+          Alert.alert("My title", "My Message", [
+            { text: 'Yes' },
+            { text: 'No' }
+          ])
+        }
+      />
     </SafeAreaView >
   );
 }
+
+const containerStyle = { backgroundColor: 'blue' };
 
 const styles = StyleSheet.create({
   container: {
